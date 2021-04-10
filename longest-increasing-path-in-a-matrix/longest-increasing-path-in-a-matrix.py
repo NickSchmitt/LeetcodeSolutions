df6bac4a2e276a28b3a5e00a6ec3e2d18dc9e1cd
Nick Schmitt <nickschmitt@gmail.com>
2021-04-10 13:09:29 -0700

    Time: 336 ms (99.38%), Space: 18.1 MB (30.83%) - LeetHub

---
+++ longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestIncreasingPath(self, M: List[List[int]]) -> int:
+        
+        ylength = len(M)
+        xlength = len(M[0])
+        arr=[]
+        
+        # recursive dfs returns longest path leading to M[y][x], caching along the way
+        @cache
+        def search_path(y, x):
+            val = M[y][x]
+            # returns the longest path of any adjacent value. Minimum will be itself (1 + max(0))
+            return 1 + max(
+                # search all adjacent values that are smaller than the current value.
+                search_path(y+1,x) if y < ylength - 1 and val > M[y+1][x] else 0,
+                search_path(y-1,x) if y > 0 and val > M[y-1][x] else 0, 
+                search_path(y,x+1) if x < xlength - 1 and val > M[y][x+1] else 0,
+                search_path(y,x-1) if x > 0 and val > M[y][x-1] else 0)
+        
+        # search path for all x and y, then return the max value
+        return max(search_path(y, x) for y in range(ylength) for x in range(xlength))
