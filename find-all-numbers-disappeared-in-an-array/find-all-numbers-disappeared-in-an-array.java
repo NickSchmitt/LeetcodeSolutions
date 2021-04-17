da246f4c45f13f9c67726336d8e1cb7176c26eac
Nick Schmitt <nickschmitt@gmail.com>
2021-04-17 14:06:06 -0700

    Time: 5 ms (78.06%), Space: 47.1 MB (95.04%) - LeetHub

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,33 +1,20 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         
-        // Iterate over each of the elements in the original array
+        List<Integer> missingIntegers = new LinkedList<Integer>();
+
+        
         for (int i = 0; i < nums.length; i++) {
-            
-            // Treat the value as the new index
-            int newIndex = Math.abs(nums[i]) - 1;
-            
-            // Check the magnitude of value at this new index
-            // If the magnitude is positive, make it negative 
-            // thus indicating that the number nums[i] has 
-            // appeared or has been visited.
-            if (nums[newIndex] > 0) {
-                nums[newIndex] *= -1;
-            }
+            int index = Math.abs(nums[i]) - 1;
+            nums[index] = -Math.abs(nums[index]);
         }
         
-        // Response array that would contain the missing numbers
-        List<Integer> result = new LinkedList<Integer>();
         
-        // Iterate over the numbers from 1 to N and add all those
-        // that have positive magnitude in the array
+
         for (int i = 1; i <= nums.length; i++) {
-            
-            if (nums[i - 1] > 0) {
-                result.add(i);
-            }
+            if (nums[i - 1] > 0) missingIntegers.add(i);
         }
         
-        return result;
+        return missingIntegers;
     }
 }
