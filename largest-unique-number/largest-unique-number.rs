19908134851c206bff3f2bb6c2f8c2f4d0b200d7
Nick Schmitt <nickschmitt@gmail.com>
2021-04-19 00:16:56 -0700

    Time: 0 ms (100.00%), Space: 2.1 MB (68.42%) - LeetHub

---
+++ largest-unique-number/largest-unique-number.rs
@@ -0,0 +1,19 @@
+use std::collections::BTreeMap;
+
+impl Solution {
+    pub fn largest_unique_number(a: Vec<i32>) -> i32 {
+        
+        let mut map = BTreeMap::new();
+        
+        for num in a {
+            *map.entry(num).or_insert(0) += 1;
+        }
+
+        *map.iter()
+            .rev()
+            .skip_while(|(_,v)|v>&&1)
+            .map(|(k, _)| k)
+            .next()
+            .unwrap_or(&-1)
+    }
+}
