98fd4d1c0ae98c99bbd49d61245b2d46b93cc4ce
Nick Schmitt <nickschmitt@gmail.com>
2021-04-24 13:16:43 -0700

    Time: 4 ms (93.34%), Space: 38.5 MB (98.90%) - LeetHub

--- longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,27 +1,19 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        /*
-        Store letter counts in an array
-        Store the mode count
-        If non-mode characters in the window exceed k, shrink the window
-        */
-        
-        int[] arr = new int[26];
-        int mode = 0, left = 0, longest = 0;
-        
-        for(int right = 0; right < s.length(); right++){
 
-            arr[s.charAt(right) - 'A']++;
-            mode = Math.max(mode, arr[s.charAt(right) - 'A']);
-            
-            if(right - left + 1 - mode > k){
-                arr[s.charAt(left) - 'A']--;
-                left++;
+        char[] charArr = new char[26];
+
+        int i = 0, longest = 0, modeCharCount = 0;
+
+        for (int j = 0; j < s.length(); j++) {
+            modeCharCount = Math.max(modeCharCount, ++charArr[s.charAt(j) - 'A']);
+            while (j - i + 1 - modeCharCount > k) {
+                charArr[s.charAt(i) - 'A']--;
+                i++;
             }
-            
-            longest = Math.max(longest, right - left + 1);
+            longest = Math.max(longest, j - i + 1);
         }
-        
+
         return longest;
     }
 }
