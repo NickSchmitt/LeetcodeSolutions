2f65a581b2f8494a8708f922e440aa905c9a96f1
Nick Schmitt <nickschmitt@gmail.com>
2021-07-19 21:12:01 -0700

    Time: 28 ms (88.72%), Space: 14.3 MB (44.17%) - LeetHub

---
+++ longer-contiguous-segments-of-ones-than-zeros/longer-contiguous-segments-of-ones-than-zeros.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkZeroOnes(self, s: str) -> bool:
+        
+        zero_counter = 0
+        max_zeroes = 0
+        one_counter = 0
+        max_ones = 0
+        
+        for i in s:
+            if i == '0':
+                one_counter = 0
+                zero_counter += 1
+                max_zeroes = max(zero_counter, max_zeroes)
+            if i == '1':
+                zero_counter = 0
+                one_counter += 1
+                max_ones = max(one_counter, max_ones)
+        return max_ones > max_zeroes
