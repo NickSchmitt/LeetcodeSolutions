997a96fa6c2d5e44867524a1ce42cb7eadfee606
Nick Schmitt <nickschmitt@gmail.com>
2021-07-20 16:10:11 -0700

    Time: 52 ms (96.52%), Space: 15.8 MB (9.29%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums, target):
+        
+        
         hash_map = {}
         
-        
         for i, num in enumerate(nums):
-            if hash_map.get(target-num):
-                return [i, hash_map.get(target-num)[0]]
+            diff = hash_map.get(target-num)
+            if diff:
+                return [i, diff[0]]
             else: hash_map[num] = (i, num)
-        
-        # return "hello"
