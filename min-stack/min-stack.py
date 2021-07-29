eeb85856770a40662c084cba3e767f1c5651157e
Nick Schmitt <nickschmitt@gmail.com>
2021-07-29 16:53:03 -0700

    Time: 40 ms (99.98%), Space: 18 MB (81.01%) - LeetHub

--- min-stack/min-stack.py
+++ min-stack/min-stack.py
@@ -1,39 +1,20 @@
-from math import inf
-
 class MinStack:
 
     def __init__(self):
         self.stack = []
-        """
-        initialize your data structure here.
-        [1, 0]
+        self.min = []
         
-        push 1, min is 1
-        
-        push 0, min is 0
-        
-        
-        """
-        
-
     def push(self, val: int) -> None:
-        new_min = val if not self.stack else min(self.getMin(), val)
-        self.stack.append((val, new_min))
+        if not self.stack or val <= self.min[-1]:
+            self.min.append(val)
+        self.stack.append(val)
 
     def pop(self) -> None:
-        return self.stack.pop()[0]
-        
+        if self.stack.pop() == self.min[-1]:
+            self.min.pop()
 
     def top(self) -> int:
-        return self.stack[-1][0]
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.stack[-1][1]
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+        return self.min[-1]
