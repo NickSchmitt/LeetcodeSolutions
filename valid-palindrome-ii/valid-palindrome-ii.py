5d4431a4b4cd9cfe606bc6a0de460c2a9713df5c
Nick Schmitt <nickschmitt@gmail.com>
2021-07-30 09:56:12 -0700

    Time: 100 ms (83.30%), Space: 14.5 MB (91.84%) - LeetHub

--- valid-palindrome-ii/valid-palindrome-ii.py
+++ valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,16 +1,14 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-
-        left=0
-        right=len(s)-1
         
-        while left < right:
-            if s[left] == s[right]:
-                left+=1
-                right-=1
-            else:
-                return s[left:right] == s[left:right][::-1] or s[left+1:right+1] == s[left+1:right+1][::-1]
+        left = 0
+        right = len(s)-1
+        
+        while left<right:
+            if s[left]!=s[right]:
+                return (s[left:right] == s[left:right][::-1] or         # checks from left to before right
+                        s[left+1:right+1] == s[left+1:right+1][::-1]   # checks after left to right
+                       )
+            left+=1
+            right-=1
         return True
-    
-    
-    
