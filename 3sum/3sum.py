3aba36bedef20acd0670a2a3d61da094aee2d083
Nick Schmitt <nickschmitt@gmail.com>
2021-07-31 09:30:45 -0700

    Time: 648 ms (96.09%), Space: 17.5 MB (74.18%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,40 +1,20 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-        """
-        sort nums
-        go through each num doing two_sum(num)
-        if it's the same as last time, skip it since we've done two sum for that very number
-        if we get above 0, just return what we've got so far        
-        """
-        nums.sort()
+
+        nums.sort() # since our search is going to be the whole array for each elem (n^2) we can afford the nlogn sort
         ans = []
         
         for idx in range(len(nums)):
-            
-            if idx == 0 or nums[idx-1] != nums[idx]:
+            if nums[idx] > 0: break                     # if num is above 0, we can't sum with new numbers to 0, so break
+            if idx == 0 or nums[idx-1] != nums[idx]:    # perform two sum only if not duplicate
                 self.twoSumII(idx, ans, nums)            
             
         return ans
-                   
-        
-        
         
             
     def twoSumII(self, idx, ans, nums):
-        """
-        sorted two_sum(idx)
         
-        target = 0-nums[idx]
-        
-        put pointers at n+1 and end
-        if larger than target, bring in right
-        if less than target, bring in left
-        """       
-        
-        target = 0-nums[idx]
-        
-        left = idx+1
+        left = idx+1                                    
         right = len(nums)-1
         
         while left < right:
@@ -47,7 +27,7 @@ class Solution:
                 ans.append([nums[idx], nums[left], nums[right]])
                 left += 1
                 right -= 1
-                while left < right and nums[left] == nums[left-1]:
-                    left+=1
+                while left < right and nums[right] == nums[right+1]: # bring in either right or left to avoid duplicates
+                    right-=1
                 
         
