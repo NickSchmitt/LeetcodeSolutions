8778185ea334a818bd1aa77015cabef54a345c8b
Nick Schmitt <nickschmitt@gmail.com>
2021-07-31 11:51:16 -0700

    Time: 72 ms (85.23%), Space: 15.7 MB (49.05%) - LeetHub

--- k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
+++ k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
@@ -4,18 +4,16 @@ class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
         
         count = 0
-        k_zero = k == 0
+        grouped = Counter(nums) if k == 0 else set(nums)
         
         if k==0:
-            freq_map = Counter(nums)
-            for num in freq_map:
-                if freq_map[num] > 1:
+            for num in grouped:
+                if grouped[num] > 1:
                     count += 1
         
         else:
-            s = set(nums)
-            for num in s:
-                if num + k in s:
+            for num in grouped:
+                if num + k in grouped:
                     count += 1
                 
 
