c48e68ed50cb6c7db053bfdc45f6159e2ff13899
Nick Schmitt <nickschmitt@gmail.com>
2021-07-31 22:19:57 -0700

    Time: 184 ms (97.34%), Space: 15.5 MB (28.62%) - LeetHub

--- shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
+++ shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
@@ -1,38 +1,18 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        """
-        2,6,4,8,10,9,15
-        
-        2 15 valid
-        2 6 9 15 valid
-        2 6 4 10 9 15 invalid
-        
-        
-        2,6,4,8,10,15
-        
-        bring things in until you find an invalid left and invalid right
-        
-        iterate that slice to find the min max
-        push left back out until min < nums[left]
-        push right back out until max > nums[right]
-        
-        
-        """
+
         length = 0
         left = 0
         right = len(nums)-1
         
         while left < right and nums[left] <= nums[left+1]:
             left+=1
-        
-        if left == right: return 0
                 
         while nums[right] >= nums[right-1]:
             if left == right: return 0
             right-=1
         
-        unsorted = nums[left:right+1]
-        unsorted_min, unsorted_max = min(unsorted), max(unsorted)
+        unsorted_min, unsorted_max = min(nums[left:right+1]), max(nums[left:right+1])
         
         
         while left > 0 and nums[left-1] > unsorted_min:
@@ -41,9 +21,6 @@ class Solution:
         while right < len(nums)-1 and nums[right+1] < unsorted_max:
             right += 1
         
-        print(nums[left])
-        print(nums[right])
-        print(nums[left:right+1])
         return len(nums[left:right+1])
         
         
