1e8c48b384e183532ae961196517c662fa0e16d3
Nick Schmitt <nickschmitt@gmail.com>
2021-08-01 18:38:04 -0700

    Time: 440 ms (87.31%), Space: 21.2 MB (15.06%) - LeetHub

--- boats-to-save-people/boats-to-save-people.py
+++ boats-to-save-people/boats-to-save-people.py
@@ -10,28 +10,5 @@ class Solution:
                 left+=1
             boats += 1
             right -= 1
-            
-        print(boats)
-        
-        # boats += -(-(len(people[left:right+1])+1) // 2)
         
         return boats
-            
-            
-            # if left and right can fit, increment boats and bring them both in
-            # if not, increment boats
-                
-        """
-        limit 5
-        2,2,3,4,5,5
-        ^left
-              ^right
-        boats 3
-        2,2,3,4,6,6
-          ^left
-            ^right
-        boats 
-        
-        2 6, bring in
-        2 4 valid
-        """
