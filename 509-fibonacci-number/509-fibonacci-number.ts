9efda760a24426d684c664d60663b7003baf1312
Nick Schmitt <nickschmitt@gmail.com>
2022-02-22 17:55:53 -0800

    Time: 950 ms (5.29%), Space: 50.9 MB (5.29%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.ts
+++ 509-fibonacci-number/509-fibonacci-number.ts
@@ -1,6 +1,11 @@
-function fib(n: number, memo = {}): number {
-    if(n <= 1) return n
-    if(n in memo) return memo[n]
-    memo[n] = fib(n-1, memo) + fib(n-2, memo)
-    return memo[n]
+function fib(n: number): number {
+    const memo = {};
+    const dp = (n) => {
+        if(n <= 1) return n
+        if(n in memo) return memo[n]
+        memo[n] = fib(n-1) + fib(n-2)
+        return memo[n]
+    }
+  
+    return dp(n)
 };
